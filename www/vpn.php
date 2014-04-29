972a9c08503ac4d3a48b27c6dfc67111d3d1a1ae
Bastian Bittorf <bittorf@bluebottle.com>
2014-04-29 15:59:53 -0400

    www: changed shell call: how many clients are active

--- www/vpn.php
+++ www/vpn.php
@@ -2,7 +2,8 @@
   $serverinfo['server'] = "vpn2.weimarnetz.de";
   $serverinfo['maxmtu'] = "1500";
   $serverinfo['port_vtun_nossl_nolzo'] = "5063";
-  $serverinfo['clients'] = trim(shell_exec("ip a|grep \"^[0-9].*tap\"|wc -l"));
+  $serverinfo['clients'] = trim(shell_exec("grep tap[0-9] /proc/net/dev | wc -l"));
+
   header('Content-Type: application/json');
   echo json_encode($serverinfo, JSON_PRETTY_PRINT);
 ?>

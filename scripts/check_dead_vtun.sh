842c8068763dfb21df6024060bf28059b44069a6
storchi <cstorchsp@gmx.net>
2020-04-15 15:03:57 +0200

    check for dead vtun connections

---
+++ scripts/check_dead_vtun.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+for i in `ls /sys/class/net/ | grep tap`; 
+ do echo ""; 
+ echo $i; 
+ val1=`cat /sys/class/net/$i/statistics/rx_packets`; 
+ echo $val1; 
+ sleep 3; 
+ val2=`cat /sys/class/net/$i/statistics/rx_packets`; 
+ echo $val2;
+ if [ $val1 -eq $val2 ]; then 
+  echo Mist; 
+  sudo kill `pgrep -f "$i "`; 
+ fi  ;
+done
